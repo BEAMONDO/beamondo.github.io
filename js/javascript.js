26b2c941948bcc0203473171e85b09e064b82fc9
David Beamonde <davidbeamonde2004@gmail.com>
2022-12-06 23:37:22 +0100

    Pokemon

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+function abrirMenu(){
+    document.getElementById('menu').innerHTML='<div class="pags"><a href="index.html">Inicio</a></div> <div class="pags"><a href="juegos.html">Juegos</a></div> <div class="pags"><a href="emuladores.html">Emuladores</a></div> <div class="pags"><a href="iniciales.html">Pokémon iniciales</a></div> <div class="pags"><a href="peliculas.html">Películas y episodios</a></div>';
+    document.getElementById('cabecera').innerHTML='<div class="m1"><img class="logo" src="imagenes/logo.PNG"></div> <div id="I1" class="m2"><h1>Juegos, películas y episodios</h1></div>';
+}
+function pieDePagina(){
+    document.getElementById('piepagina').innerHTML='- @David Beamonde Salinas 2022-23'
+}
